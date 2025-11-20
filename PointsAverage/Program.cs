cfed5f5a745f47907fd7b469fdee1455f5b0cfb6
bogdogdan-cmd <bogdogdan@gmail.com>
2025-11-20 12:39:22 +0200

    Додано  PointsAverage

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+double sum = 0;
+for (int i = 1; i <= 5; i++) {
+    Console.Write("Ведіть оцінку:");
+    double Bal = Convert.ToDouble(Console.ReadLine());
+    sum += Bal; 
+}
+double averge = sum / 5;
+Console.WriteLine($"Середній бал: {averge}");
+
+if (averge >= 50)
+{
+    Console.WriteLine("Студент допущений до екзамену.");
+}
+else {
+    Console.WriteLine("Студент не допущений до екзамену.");
+}
